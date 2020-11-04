47bcae041c59ae46b27356cc740dc65439b75346
Mauricio Queiros <mauriciobqueiros@hotmail.com>
2020-11-03 21:30:29 -0300

    inserindo .iml no .gitignore

--- src/test/java/br/com/inmetrics/teste/steps/CadastroUsuarioSteps.java
+++ src/test/java/br/com/inmetrics/teste/steps/CadastroUsuarioSteps.java
@@ -33,6 +33,22 @@ public class CadastroUsuarioSteps {
         System.out.println("entao");
     }
 
+    @Quando("^tentar cadastrar um usuario com menos de 8 caracteres$")
+    public void tentar_cadastrar_um_usuario_com_menos_de_caracteres() throws Throwable {
+    }
+
+    @Entao("^o sistema apresenta um aviso de que o nome do usuario precisa de no minimo 8 caracteres$")
+    public void o_sistema_apresenta_um_aviso_de_que_o_nome_do_usuario_precisa_de_no_minimo_caracteres() throws Throwable {
+    }
+
+    @Quando("^inserir senhas divergentes nos campos de senha$")
+    public void inserir_senhas_divergentes_nos_campos_de_senha() throws Throwable {
+    }
+
+    @Entao("^o sistema apresenta a mensagem Senhas nao combinam$")
+    public void o_sistema_apresenta_a_mensagem_Senhas_nao_combinam() throws Throwable {
+    }
+
     @Before
     public void init(){
         System.out.println("Aqui Before");
